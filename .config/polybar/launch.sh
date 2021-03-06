290b28b6e64e9bb66bd1dfc6ea376a91401b9764
uwumouse <pythonisajoke@gmail.com>
2021-03-06 22:14:13 +0300

    Got rid of links

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,1 +1,12 @@
-/home/mouse/Dropbox/.config/polybar/launch.sh
+#!/usr/bin/env bash
+
+# Terminate already running bar instances
+killall -q polybar
+# If all your bars have ipc enabled, you can also use 
+# polybar-msg cmd quit
+
+# Launch bar1 and bar2
+echo "---" | tee -a /tmp/polybar.bottom.log /tmp/polybar.top.log
+polybar bottom >> /tmp/polybar.bottom.log 2>&1 & disown
+polybar top >> /tmp/polybar.top.log 2>&1 & disown
+echo "Bars launched..."
